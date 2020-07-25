4199a7ced036a076208007c9ec0c8a753914deea
Brennan Neilson <bvneilson@gmail.com>
2020-07-24 18:57:11 -0600

    implement linked list

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,84 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_next(self, new_next):
+        self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add_to_head(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
+        if self.length == 0:
+            self.tail = new_node
+        self.length += 1
+
+    def add_to_tail(self, value):
+        new_node = Node(value)
+        if self.head is None and self.tail is None:
+            self.head = new_node
+        else:
+            self.tail.set_next(new_node)
+        self.tail = new_node
+        self.length += 1
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        elif self.head == self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            self.length -= 1
+            return value
+        else:
+            value = self.head.get_value()
+            self.head = self.head.get_next()
+            self.length -= 1
+            return value
+
+    def remove_tail(self):
+        if self.head is None:
+            return None
+        elif self.tail == self.head:
+            value = self.tail.get_value()
+            self.tail = self.tail.get_next()
+            self.length -= 1
+            return value
+
+    def contains(self, value):
+        if self.head is None:
+            return None
+        found = False
+        cur_node = self.head
+        cur_val = self.head.get_value()
+        while cur_node is not None:
+            if cur_node.get_value() == value:
+                found = True
+            cur_node = cur_node.get_next()
+
+        return found
+
+    def get_max(self):
+        if self.head is None:
+            return None
+        cur_node = self.head
+        cur_max = self.head.get_value()
+        while cur_node is not None:
+            if cur_node.get_value() > cur_max:
+                cur_max = cur_node.get_value()
+            cur_node = cur_node.get_next()
+
+        return cur_max
